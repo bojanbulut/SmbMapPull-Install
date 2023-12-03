cdf2d4d62511e89651ae9c9bce45f13bc68e35e1
bojanbulut <bojanbulut@gmail.com>
2023-12-03 02:59:40 -0500

    Add files via upload

---
+++ AAD updated.ps1
@@ -0,0 +1,16 @@
+﻿
+Set-ExecutionPolicy Bypass #Permissions 
+
+Install-PackageProvider -Name NuGet -Force #install dependency
+
+$Azpath= "C:\Program Files\WindowsPowerShell\Modules\Az"
+$pathbool = Test-path -Path $Azpath
+
+if ($pathbool -eq $false) {Install-Module -Name AzureRM -Force; Write-Host("Azure Module has been installed")
+
+}elseif(Write-Host "AzureRM is already installed"){
+}
+
+Start-Sleep -Seconds 10
+
+exit 
